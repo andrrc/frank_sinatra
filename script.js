92047522b78a032fd9ae8b1570a6465c7dbc44f4
andrrc <andrevieiracvf@gmail.com>
2025-03-24 00:07:38 -0300

    Organizando a estrutas das pastas, e arrumando o codigo da musica.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const imgDisco = document.getElementById('img-disco');
-const audioMusic = document.getElementById('audio-music');
+const imgDisco = document.getElementById("img-disco");
+const audioMusic = document.getElementById("audio-music");
 
-imgDisco.addEventListener('click',ativarMusica);
+imgDisco.addEventListener("click", ativarMusica);
 
-function ativarMusica(){
-    console.log("AODIAO")
+var StartMusic = 0;
+function ativarMusica() {
+  if ((StartMusic == 0)) {
     audioMusic.play();
     imgDisco.style.animation = "spin 5s linear infinite";
+    StartMusic = 1;
+  } else {
+    audioMusic.pause();
+    imgDisco.style.animation = "none";
+    StartMusic = 0;
+  }
 }
